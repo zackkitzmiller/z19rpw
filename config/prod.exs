f603fd59bba11e7adcf2117350e5b9b457d156f1
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-09-07 14:35:44 -0500

    run openly

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :z19rpw, Z19rpwWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "0.0.0.0", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
