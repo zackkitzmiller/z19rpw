c21b56473131f1ca043fff14b63aed2e4e9767f6
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-09-28 06:41:24 -0500

    config: add staging configuration

---
+++ config/staging.exs
@@ -0,0 +1,30 @@
+use Mix.Config
+
+config :z19rpw, Z19rpw.Repo,
+  username: "root",
+  password: "",
+  database: "defaultdb",
+  hostname: "localhost",
+  port: 26257,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
+config :z19rpw, Z19rpwWeb.Endpoint,
+  http: [
+    host: "http://z19rpw.stag",
+    port: System.get_env("PORT") || 4000],
+  debug_errors: true,
+  code_reloader: false,
+  check_origin: false,
+  watchers: [
+    node: [
+      "node_modules/webpack/bin/webpack.js",
+      "--mode",
+      "development",
+      "--watch-stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
+
+config :logger, :console, format: "[$level] $message\n"
+config :phoenix, :stacktrace_depth, 20
