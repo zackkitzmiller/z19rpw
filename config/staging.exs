16adec6df6329a89064ae38150fd6aae146951f0
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-09-28 22:30:55 -0500

    docker: minors

--- config/staging.exs
+++ config/staging.exs
@@ -4,18 +4,19 @@ config :z19rpw, Z19rpw.Repo,
   username: "root",
   password: "",
   database: "defaultdb",
-  hostname: "localhost",
+  hostname: "cockroachdb-public.default.svc.cluster.local",
   port: 26257,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
 config :z19rpw, Z19rpwWeb.Endpoint,
-  url: [host: "z19rpw.stag"],
-  http: [port: String.to_integer(System.get_env("PORT") || "4000")],
+  url: [host: "z19rpw.stag", 80],
+  http: [port: "4000"],
   debug_errors: true,
   code_reloader: false,
   check_origin: false
 
 config :logger, :console, format: "[$level] $message\n"
+config :logger, level: :debug
 config :phoenix, :stacktrace_depth, 20
 config :z19rpw, Z19rpwWeb.Endpoint, server: true
