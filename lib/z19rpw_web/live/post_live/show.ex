9474bc508147bd3e31081bd54e1d188497d27ab3
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-10-01 23:21:42 -0500

    blog: update post show view

--- lib/z19rpw_web/live/post_live/show.ex
+++ lib/z19rpw_web/live/post_live/show.ex
@@ -5,6 +5,8 @@ defmodule Z19rpwWeb.PostLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket), do: Blog.subscribe()
+
     {:ok, socket}
   end
 
@@ -18,4 +20,9 @@ defmodule Z19rpwWeb.PostLive.Show do
 
   defp page_title(:show), do: "Show Post"
   defp page_title(:edit), do: "Edit Post"
+
+  @impl true
+  def handle_info({:post_updated, post}, socket) do
+    {:noreply, update(socket, :post, fn _ -> post end)}
+  end
 end
