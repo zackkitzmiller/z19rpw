b40d171e39d5ee27011f7a52e43e75284f49f4c4
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-10-01 23:03:17 -0500

    deps: remove specialized build for file_system

--- mix.exs
+++ mix.exs
@@ -51,7 +51,6 @@ defmodule Z19rpw.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:libcluster, "~> 3.0"},
       {:timex, "~>3.5"},
-      {:file_system, git: "https://github.com/falood/file_system.git", override: true},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:earmark, "~> 1.4.10"}
