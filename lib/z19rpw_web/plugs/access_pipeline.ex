ad22f08b5d63e8d0eb1aea6fc9aa9d826d9a33bd
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-10-03 00:32:57 -0500

    code: remove old auth code

--- lib/z19rpw_web/plugs/access_pipeline.ex
+++
@@ -1,8 +0,0 @@
-defmodule Z19rpwWeb.Plug.AuthAccessPipeline do
-  use Guardian.Plug.Pipeline, otp_app: :z19rpw
-
-  plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
-  plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
-  plug Guardian.Plug.EnsureAuthenticated
-  plug Guardian.Plug.LoadResource, ensure: true
-end
