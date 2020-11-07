bb34a3df19f2fdc917f1142dcff98914a90e84cd
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-11-06 23:23:36 -0600

    chore: remove comment

--- lib/z19rpw_web/live/live_helpers.ex
+++ lib/z19rpw_web/live/live_helpers.ex
@@ -1,20 +1,6 @@
 defmodule Z19rpwWeb.LiveHelpers do
   import Phoenix.LiveView.Helpers
 
-  @doc """
-  Renders a component inside the `Z19rpwWeb.ModalComponent` component.
-
-  The rendered modal receives a `:return_to` option to properly update
-  the URL when the modal is closed.
-
-  ## Examples
-
-      <%= live_modal @socket, Z19rpwWeb.PostLive.FormComponent,
-        id: @post.id || :new,
-        action: @live_action,
-        post: @post,
-        return_to: Routes.post_index_path(@socket, :index) %>
-  """
   def live_modal(socket, component, opts) do
     path = Keyword.fetch!(opts, :return_to)
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
