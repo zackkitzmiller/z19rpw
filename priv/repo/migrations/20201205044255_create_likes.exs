e3fee7112f94393b6b7b7c0445d0f6eeee98fa5e
Zack Kitzmiller <zackkitzmiller@gmail.com>
2020-12-04 23:43:10 -0600

    like types

--- priv/repo/migrations/20201205044255_create_likes.exs
+++ priv/repo/migrations/20201205044255_create_likes.exs
@@ -4,8 +4,8 @@ defmodule Z19rpw.Repo.Migrations.CreateLikes do
   def change do
     create table(:likes, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :post_id, references(:posts, on_delete: :nothing, type: :integer)
-      add :user_id, references(:users, on_delete: :nothing, type: :integer)
+      add :post_id, :integer, references(:posts, on_delete: :nothing, type: :integer)
+      add :user_id, :integer, references(:users, on_delete: :nothing, type: :integer)
 
       timestamps()
     end
