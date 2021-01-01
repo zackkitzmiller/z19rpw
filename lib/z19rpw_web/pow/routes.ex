50fbc4cac6a0994bb63a035a378535f95e76929b
Zack Kitzmiller <zackkitzmiller@gmail.com>
2021-01-01 10:56:41 -0600

    auth(email confirmation): redirect to the signin page before confirming

---
+++ lib/z19rpw_web/pow/routes.ex
@@ -0,0 +1,9 @@
+defmodule Z19rpwWeb.Pow.Routes do
+  @moduledoc """
+  handle callbacks for Pow routes
+  """
+  use Pow.Phoenix.Routes
+  alias Z19rpwWeb.Router.Helpers, as: Routes
+
+  def after_sign_in_path(conn), do: Routes.pow_session_path(conn, :new)
+end
