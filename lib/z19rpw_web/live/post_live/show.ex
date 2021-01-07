2208125cfe9016585e2f5dc54a2156d7938dc570
Zack Kitzmiller <zackkitzmiller@gmail.com>
2021-01-06 23:53:53 -0600

    blog(error): temporarily handle 404s
    refs #112

--- lib/z19rpw_web/live/post_live/show.ex
+++ lib/z19rpw_web/live/post_live/show.ex
@@ -18,12 +18,15 @@ defmodule Z19rpwWeb.PostLive.Show do
   @impl true
   def handle_params(%{"slug" => slug}, _, socket) do
     post = Blog.get_post_by_slug!(slug)
+    page_title = if is_nil(post), do: "z19r - post not found", else: post.title
 
-    {:noreply,
-     socket
-     |> assign(:current_user, socket.assigns.current_user)
-     |> assign(:post, post)
-     |> assign(:page_title, post.title)}
+    socket =
+      socket
+      |> assign(:current_user, socket.assigns.current_user)
+      |> assign(:post, post)
+      |> assign(:page_title, page_title)
+
+    {:noreply, socket}
   end
 
   @impl true
