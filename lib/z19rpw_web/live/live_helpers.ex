8bb773fed7411196be6e6b401e07094daf963039
Zack Kitzmiller <zackkitzmiller@gmail.com>
2021-02-14 19:08:24 -0600

    fix(lint): we dont use the socket here, but need it for API conformity

--- lib/z19rpw_web/live/live_helpers.ex
+++ lib/z19rpw_web/live/live_helpers.ex
@@ -2,9 +2,9 @@ defmodule Z19rpwWeb.LiveHelpers do
   @moduledoc false
   import Phoenix.LiveView.Helpers
 
-  def live_modal(socket, component, opts) do
+  def live_modal(_socket, component, opts) do
     path = Keyword.fetch!(opts, :return_to)
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
-    live_component(socket, Z19rpwWeb.ModalComponent, modal_opts)
+    live_component(_socket, Z19rpwWeb.ModalComponent, modal_opts)
   end
 end
